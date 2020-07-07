85a2eba3eebd357c55ed5cd8e92991df6b10ccf1
exxxar <exxxar@gmail.com>
2020-07-07 20:29:26 +0300

    fixes

--- config/app.php
+++ config/app.php
@@ -160,6 +160,8 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         App\Providers\BotMan\DriverServiceProvider::class,
+        Telegram\Bot\Laravel\TelegramServiceProvider::class
+
     ],
 
     /*
@@ -208,6 +210,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Telegram'  => Telegram\Bot\Laravel\Facades\Telegram::class
+
 
     ],
 
